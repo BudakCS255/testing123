503731edfc8611bac681fc159207a6c1169499df
BudakCS255 <148829262+BudakCS255@users.noreply.github.com>
2023-11-10 00:26:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Bilik 4312</title>
+</head>
+<body>
+    <h1>Welcome to Bilik 4312!</h1>
+    <p>Kita ada Afnan, Fauzan dan Zuhair.</p>
+</body>
+</html>
